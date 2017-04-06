e983c34c2af53a6806120a5ac311fdd5391a7f1a
Joachim Wyslutsky <jnw41@zips.uakron.edu>
2017-04-06 00:18:21 -0400

    Add files via upload

--- issTrack.cpp
+++ issTrack.cpp
@@ -10,41 +10,44 @@ int main(int argc, char*argv[])
     int issueNum;
     std::string description, status;
 
-    if(argv[1]=="c") myIssue.createIssue();
+    //std::cout<<"Number of arguments: "<<argc<<std::endl<<"Argument Entered: " << *argv[1]<<std::endl;
 
-    else if(argv[1]=="s")
+    if(*argv[1]=='n') myIssue.createIssue();
+
+    else if(*argv[1]=='s')
     {
         std::cout<<"Please enter an issue number you are checking the status of: ";
         std::cin>>issueNum;
         myIssue.checkStatus(issueNum);
     }
 
-    else if(argv[1]=="f")
+    else if(*argv[1]=='f')
     {
         std::cout<<"Please enter an issue number you wish to find: ";
         std::cin>>issueNum;
         myIssue.findIssue(issueNum);
     }
 
-    else if(argv[1]=="cs")
+    else if(*argv[1]=='c')
     {
         std::cout<<"Please enter an issue number: ";
         std::cin>>issueNum;
         myIssue.checkStatus(issueNum);
 
         std::cout<<"Please enter the new status of this issue: ";
-        std::getline(std::cin,status);
+        std::cin.ignore();
+        std::getline(std::cin, status);
         myIssue.changeStatus(issueNum,status);
     }
 
-    else if(argv[1]=="e")
+    else if(*argv[1]=='e')
     {
         std::cout<<"Please enter a status: ";
         std::getline(std::cin,status);
         myIssue.getExistingIssues(status);
     }
 
-    else if(argv[1]=="u")
+    else if(*argv[1]=='u')
     {
         std::cout<<"Please enter an issue number: ";
         std::cin>>issueNum;
--- issue.h
+++ issue.h
@@ -7,22 +7,32 @@
 #include "rapidjson/prettywriter.h"
 
 #include <iostream>
+#include <ctime>
+#include <iomanip>
 #include <fstream>
 #include <string>
-#include <cstdio>
 
 using namespace rapidjson;
 
 class Issue
 {
 private:
+        std::string documentIn;
         std::ifstream issueIn,
         issueListIn; //JSON File Storing List of Issues by status
         std::ofstream issueOut,
         issueListOut; //JSON File Storing List of Issues by status
 
         //Necessary Functions for Issue Class
-        int getNextNumber(); //Gets the next sequential Issue Number
+        int getNextNumber(Document &document) //Gets the next sequential Issue Number
+        {
+            int num;
+            Value& nxtIssueNum = document["Next Number"];
+            num = nxtIssueNum.GetInt();
+
+            nxtIssueNum = nxtIssueNum.GetInt() + 1;
+
+        }
 public:
         //User Functions
         void checkStatus(int); //Check the Status of a user given Issue Number
@@ -47,6 +57,76 @@ void Issue::changeStatus(int issueNum, std::string status)
 
 void Issue::createIssue()
 {
+    Document inDocument, newIssue;
+
+    std::ifstream issueIn;
+    std::string fileName="issues.json";
+
+    issueIn.open(fileName.c_str(),std::ios::binary);
+
+    std::streampos begin, end;
+
+    begin=issueIn.tellg();
+    issueIn.seekg(0,std::ios::end);
+    end=issueIn.tellg();
+
+    std::streampos size = end-begin;
+    issueIn.seekg(0,std::ios::beg);
+
+    char * fileData = new char[size];
+    issueIn.read(fileData, size);
+    fileData[size]='\0';
+    issueIn.close();
+
+    inDocument.Parse(fileData);
+
+    Value& nxtIssueNum = inDocument["Next Number"];
+
+    std::cout<< "Number before function: "<<nxtIssueNum.GetInt()<<std::endl;
+
+    int nxtNum = getNextNumber(inDocument);
+
+    std::cout<< "Number after function: "<<nxtIssueNum.GetInt()<<std::endl;
+
+    fileName="newIssue.json";
+    issueIn.open(fileName.c_str(),std::ios::binary);
+
+    begin=issueIn.tellg();
+    issueIn.seekg(0,std::ios::end);
+    end=issueIn.tellg();
+
+    size = end-begin;
+    issueIn.seekg(0,std::ios::beg);
+
+    char * issueFileData = new char[size];
+    issueIn.read(issueFileData, size);
+    issueFileData[size]='\0';
+    issueIn.close();
+
+    newIssue.Parse(issueFileData);
+
+    Value& issueNum = newIssue["Number"];
+
+    issueNum.SetInt(nxtNum);
+
+    auto t = time(nullptr);
+    auto tm = *std::localtime(&t);
+
+    int month = tm.tm_mon + 1;
+    int year = tm.tm_year + 1900;
+    int day = tm.tm_mday;
+
+    std::string date = std::to_string(month)+"-"+std::to_string(day)+"-"+std::to_string(year);
+    std::cout<<"Today's Date: "<<date<<std::endl;
+
+    Value& issueDate = newIssue["Date"];
+    issueDate.SetString(date.c_str(), date.length());
+
+    std::string status = "Open";
+
+    Value& issueStatus = newIssue["Status"];
+    issueStatus.SetString(status.c_str(), status.length());
+
     return;
 }
 
